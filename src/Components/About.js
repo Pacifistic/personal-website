f5d8436bdb1cf2d4c23b737351cafccd6a023189
Dominykas Sipelis <DominykasSipelis@gmail.com>
2022-02-14 17:17:33 -0600

    swapped sample icon

--- src/Components/About.js
+++ src/Components/About.js
@@ -32,8 +32,8 @@ export default function About() {
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
                         className="object-cover object-center rounded"
-                        alt="sample toucan"
-                        src="./sample_image_toucan.jpg"
+                        alt="quest"
+                        src="./quest_icon.png"
                     />
                 </div>
             </div>
