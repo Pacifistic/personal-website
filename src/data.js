e7f81d93e183a6477668288801f13e95cee000ce
Dominykas Sipelis <DominykasSipelis@gmail.com>
2022-05-19 15:15:55 -0500

    added additional data

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 export const skills = [
     "Java Springboot",
+    "Angular",
     "AWS EC2",
     "AWS S3",
     "AWS RDS",
@@ -7,8 +8,8 @@ export const skills = [
     "AWS Lambda",
     "AWS APIGateway",
     "AWS Connect",
+    "Terraform",
     "React",
-    "Unix",
     "Git"
 ];
 
@@ -21,4 +22,23 @@ export const platforms = [
         name: "LinkedIn",
         link: "https://www.linkedin.com/in/dominykas-sipelis/"
     }
+]
+
+export const certs = [
+    {
+        name: "AWS Certified DevOps Engineer – Professional",
+        link: "https://www.credly.com/badges/47af7820-18e2-4a8e-84b6-9a51721fcb3a"
+    },
+    {
+        name: "HashiCorp Certified: Terraform Associate",
+        link: "https://www.credly.com/badges/8817318f-0d3b-480b-9978-ecb1f5867765"
+    },
+    {
+        name: "AWS Certified Developer – Associate",
+        link: "https://www.credly.com/badges/9703cbc0-2ce0-475e-8a14-41361a769947"
+    },
+    {
+        name: "AWS Certified Solutions Architect – Associate",
+        link: "https://www.credly.com/badges/ea3fc04c-5b5e-4582-804b-cab19c0a3a6a"
+    },
 ]
